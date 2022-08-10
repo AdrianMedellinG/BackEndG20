1b5694cd4bfe4eb575e95d68b136a4fd01a5f246
AdrianMedellinG <adrian.medellin.garcia@hotmail.com>
2022-08-09 22:07:54 -0500

    wip clase02

--- clase02.js
+++ clase02.js
@@ -1,6 +1,20 @@
-var colors = require('colors');
+/*
+const colors = require('colors');
 
 const arcoiris = (texto) =>{
     return texto.rainbow
 }
 console.log(arcoiris(process.argv[2])); // rainbow
+*/
+
+
+const fs = require('fs');
+const colors = require('colors');
+
+fs.writeFile('message.txt', 'texto de data', 'utf8', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!'.red);
+  });
+
+
+ 
