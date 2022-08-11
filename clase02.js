34cf87c2b0de09e1496422b94c18e42e5da85851
AdrianMedellinG <adrian.medellin.garcia@hotmail.com>
2022-08-10 19:19:36 -0500

    tarea clase02

--- clase02.js
+++ clase02.js
@@ -7,7 +7,7 @@ const arcoiris = (texto) =>{
 console.log(arcoiris(process.argv[2])); // rainbow
 */
 
-
+/*
 const fs = require('fs');
 const colors = require('colors');
 
@@ -15,6 +15,18 @@ fs.writeFile('message.txt', 'texto de data', 'utf8', (err) => {
     if (err) throw err;
     console.log('The file has been saved!'.red);
   });
+*/
 
+//! Tarea
+const fs = require('fs');
+//console.log("fs",fs.readFile)
+
+fs.readFile("message.txt","utf8", (err, data)=>{
+    if(err){
+        console.log("error",err)
+    }else{
+        console.log("data",data)
+    }
+})
 
  
