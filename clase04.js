2f2068aa70283f7fb3608ef9775a99e43c6a4b45
AdrianMedellinG <adrian.medellin.garcia@hotmail.com>
2022-08-11 21:50:15 -0500

    clase04 Promesas

--- clase04.js
+++ clase04.js
@@ -7,16 +7,10 @@
  * 3 - Preparacion de la masa
  * 4 - Hornear el pastel
  * 5 - Decorar el paster
- */
+*/
 
- const pastel = {
-    recetaLeida : false,
-    ingredientesConseguidos: false,
-    masaPreparada: false,
-    pastelHorneado: false,
-    pastelDecorado: false
-}
 
+/*
 
 const hacerPastel = (recetaSeHaLeido, callback) => {
     let error = null;
@@ -129,3 +123,145 @@ hacerPastel(pastel, (error, recetaSeHaLeido) => {
             });
     });
 });
+*/
+
+
+//! Promesas
+
+const pastel = {
+   recetaLeida : false,
+   ingredientesConseguidos: false,
+   masaPreparada: false,
+   pastelHorneado: false,
+   pastelDecorado: false
+}
+
+
+const hacerPastel = (recetaSeHaLeido) => {
+    return new Promise((resolve,reject) => {
+   
+    setTimeout(() => {
+        // Voy a leer la receta
+        recetaSeHaLeido.recetaLeida = true;
+
+        // Si no se leyo la receta, lanzar un error
+        if (!recetaSeHaLeido.recetaLeida) {
+            reject = "No ha leido la receta";
+        }
+        
+        resolve(recetaSeHaLeido)
+    }, 1000);
+});
+
+};
+
+const ingredientes = (siHayIngredientes) => {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            siHayIngredientes.ingredientesConseguidos = true;
+
+            // No  hay ingrdientes, lanzar un error
+            if (!siHayIngredientes.ingredientesConseguidos) {
+                reject = "No hay ingredientes";
+            }
+
+            resolve(siHayIngredientes)
+        }, 1000);
+    });
+};
+
+const masa = (masaYaPreparada) => {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            masaYaPreparada.masaPreparada = true;
+
+            // No esta la masa, lanzar un error
+            if (!masaYaPreparada.masaPreparada) {
+                reject = "No se ha cocido";
+            }
+
+            resolve(masaYaPreparada)
+        }, 1000);
+    });
+};
+
+const horneadoPastel = (estaPastelYaSeCocio) => {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            estaPastelYaSeCocio.pastelHorneado = true;
+
+            // No se ha horneado el pastel, lanzar un error
+            if (!estaPastelYaSeCocio.pastelHorneado) {
+                reject = "No ha horneado el pastel";
+            }
+
+            resolve(estaPastelYaSeCocio)
+        }, 1000);
+    });
+};
+
+const decorandoPastel = (pastelYaDecorado) => {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            pastelYaDecorado.pastelDecorado = true;
+
+            // No se ha horneado el pastel, lanzar un error
+            if (!pastelYaDecorado.pastelDecorado) {
+                reject = "No ha decorado el pastel";
+            }
+
+            resolve(pastelYaDecorado)
+        }, 1000);
+    });
+};
+
+
+
+const hagamosUnPastel = hacerPastel({ ...pastel });
+hagamosUnPastel
+    .then((recetaLeida) => {
+        console.log(recetaLeida);
+
+        ingredientes({ ...recetaLeida })
+        .then((ingredientesConseguidos) => {
+            console.log(ingredientesConseguidos);
+    
+            masa({ ...ingredientesConseguidos })
+            .then((masaYaPreparada) => {
+                console.log(masaYaPreparada);
+
+
+                horneadoPastel({ ...masaYaPreparada })
+                .then((pastelHorneado) => {
+                    console.log(pastelHorneado);
+                    
+                    decorandoPastel({ ...pastelHorneado })
+                    .then((pastelYaDecorado) => {
+                        console.log("EXITO!!",pastelYaDecorado);
+        
+                    })
+                    .catch((error) => {
+                        console.log("error", error);
+                    });
+                    
+                })
+                .catch((error) => {
+                    console.log("error", error);
+                });
+            })
+            .catch((error) => {
+                console.log("error", error);
+            });
+        })
+        .catch((error) => {
+            console.log("error", error);
+        });
+
+    })
+    .catch((error) => {
+        console.log("error", error);
+    });
