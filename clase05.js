c2617fbc5a25a8a2e799ee68a9a2f110e8c2598e
AdrianMedellinG <adrian.medellin.garcia@hotmail.com>
2022-08-12 22:06:11 -0500

    Clase05 Servers

--- clase05.js
+++ clase05.js
@@ -1,5 +1,5 @@
 //! Async/Await
-
+/*
 const pastel = {
     recetaLeida : false,
     ingredientesConseguidos: false,
@@ -65,17 +65,60 @@ const decorado = (decoraPastel) => {
 const leiRecetaCompleta = async () => {
     try {
         const recetaLeida = await leiReceta({...pastel})
-        console.log("Leiste receta", recetaLeida);
+        //console.log("Leiste receta", recetaLeida);
         const completasteIngredientes = await ingredientesListos({...recetaLeida})
-        console.log("Tienes todos los ingredientes", completasteIngredientes);
+        //console.log("Tienes todos los ingredientes", completasteIngredientes);
         const masaCompleta = await amasar({...completasteIngredientes})
-        console.log("La masa esta lista", masaCompleta);
+        //console.log("La masa esta lista", masaCompleta);
         const hornoPrecalentado = await horneado({...masaCompleta})
-        console.log("El pastel esta horneado", hornoPrecalentado);
+        //console.log("El pastel esta horneado", hornoPrecalentado);
         const decoracionPastel = await decorado({...hornoPrecalentado})
-        console.log("A comer!, el pastel esta listo!!!", decoracionPastel)
+        //console.log("A comer!, el pastel esta listo!!!", decoracionPastel)
     } catch (err) {
         console.log("fallo un paso", err);
     }
 }
 leiRecetaCompleta()
+*/
+
+
+//! Server HTTP
+/*
+const http = require("http")
+
+const server = http.createServer((request, response)=>{
+    //console.log("response", request.method)
+    //console.log("response", request.url)
+    response.write("Hola, bienvenido a mi servidor")
+    response.end()
+
+
+
+})
+
+server.listen(8181, ()=>{
+    console.log("Server Listening")
+})
+*/
+
+//! Server EXPRESS
+const express = require("express")
+const app = express()
+
+
+app.get("/",(request, response)=>{
+    response.write("Hola, bienvenido al get de Home")
+    response.end()
+
+})
+
+app.get("/profile",(request, response)=>{
+    response.write("Hola, bienvenido al get de PROFILE")
+    response.end()
+
+})
+   
+
+app.listen(4040, ()=>{
+    console.log("Server Listening with EXPRESS")
+})
